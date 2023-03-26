48e0a05563fac9af8150fda5e3e6d53d32a85bdb
Nishantkgit <nkathpalia2002@gmail.com>
2023-03-26 17:38:18 +0530

    added console

--- src/App.js
+++ src/App.js
@@ -1,80 +1,50 @@
-import imag from './cloud.png';
-import './App.css';
-import React, {useState} from 'react';
-// import ReactS3 from 'react-s3'
-import S3 from 'react-aws-s3'
+import AWS from 'aws-sdk';
+import { useState } from 'react';
+AWS.config.update({
+  accessKeyId: 'AKIARPM7H5Z25L5NL2QY',
+  secretAccessKey: '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',
+  region: 'us-east-1',
+  signatureVersion: 'v4',
+});
 
-const config={
-  accessKeyId:'AKIARP76KWTXZEGRKVXD',
-  secretAccessKey:'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',
-  region:'ap-south-1',
-  bucketName:'miniproj1',
-}
-function App() {
-  var nme = "Madhavan"
-  const [file, setFile] = useState()
-  const handleFileInput = (e) => {
+export default function ImageUploader() {
+  const s3 = new AWS.S3();
+  const [imageUrl, setImageUrl] = useState(null);
+  const [file, setFile] = useState(null);
+
+  const handleFileSelect = (e) => {
     setFile(e.target.files[0]);
   }
-
-  const uploadfile=async(file)=> {
-    const ReactS3Client = new S3(config);
-    ReactS3Client.uploadFile(file,file.name).then(data=>{
-      console.log(data.location)
-    })
+  const uploadToS3 = async () => {
+    if (!file) {
+      console.log("Hello");
+      alert('Hello');
+      return;
+    }
+    const params = { 
+      Bucket: 'miniproj12', 
+      Key: `${Date.now()}.${file.name}`, 
+      Body: file 
+    };
+    const { Location } = await s3.upload(params).promise();
+    setImageUrl(Location);
+    console.log('uploading to s3', Location);
   }
-  // function handleSubmit(event) {
-  //   event.preventDefault()
-  //   const url = 'http://localhost:3000/uploadFile';
-  //   const formData = new FormData();
-  //   formData.append('file', file);
-  //   formData.append('fileName', file.name);
-  //   const config = {
-  //     headers: {
-  //       'content-type': 'multipart/form-data',
-  //     },
-  //   };
-  //   axios.post(url, formData, config).then((response) => {
-  //     console.log(response.data);
-  //   });
-// }
-
   return (
-    <div className="App">
-      <h1>Welcome {nme}</h1>
-      <div className='box'>
-        <div className="card one">
-          <img src={imag} alt="" />
-          <div className="container">
-            <h4><b>Upload</b></h4>
-            <form id="fileform" >
-              <input type="file" id="myFile" name="filename"onChange={handleFileInput}/>
-              <div className='btn'>
-                <input type="submit" id='subbtn' onClick={()=>uploadfile(file)}/>
-              </div>
-            </form>
-          </div>
+    <div style={{ marginTop: '150px' }}>
+      <h1>Test Image Upload</h1>
+      <input type="file" onChange={handleFileSelect} />
+      {file && (
+        <div style={{ marginTop: '10px' }}>
+          <button onClick={uploadToS3}>Upload</button>
         </div>
-        <div className="vertical"></div>
-        <div className='boxr'>
-          <div className="card two">
-            <img src={imag} alt="" />
-            <div className="container">
-              <h4><b>Retrieve</b></h4>
-              <form id=''>
-                <p>Link</p>
-                <input type="text" id="txt" />
-                <div className='btn'>
-                  <input type="submit" id='subbtn' />
-                </div>
-              </form>
-            </div>
-          </div>
+      )}
+      {imageUrl && (
+        <div style={{ marginTop: '10px' }}>
+          <img src={imageUrl} alt="uploaded" />
         </div>
-      </div>
+      )}
     </div>
   );
 }
-export default App;
-
 
